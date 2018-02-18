b3769efb2d29bd11e2f9243609f217e033005773
Sesha <seshasaicharla@gmail.com>
2018-02-18 17:18:58 +0530

    reverted back to original

--- chap1/f_c.c
+++ chap1/f_c.c
@@ -4,7 +4,7 @@
  * for fahr = 0, 20, ...., 300*/
 main()
 {
-    float fahr, celsius;
+    int fahr, celsius;
     int lower, upper, step;
 
     lower = 0;  /*lower limit of the temperature table */
@@ -13,8 +13,8 @@ main()
 
     fahr = lower;
     while (fahr <= upper){
-        celsius = 5.0/9.0 * (fahr-32.0);
-        printf("%3.0f\t%6.1f\n", fahr, celsius);
+        celsius = 5 * (fahr-32) / 9;
+        printf("%3d\t%6d\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
