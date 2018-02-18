11b1950c9cd40aca780c1ba36cbb329343ca3724
Sesha <seshasaicharla@gmail.com>
2018-02-18 17:02:46 +0530

    Added a heading for the tabe.

--- chap1/f_c_flt.c
+++ chap1/f_c_flt.c
@@ -12,9 +12,10 @@ main()
     step = 20;  /*step size*/
 
     fahr = lower;
+    printf("Farenheit\tCelsius\n");
     while (fahr <= upper){
         celsius = 5.0/9.0 * (fahr-32.0);
-        printf("%3.0f\t%6.1f\n", fahr, celsius);
+        printf("%3.0f\t\t%6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
