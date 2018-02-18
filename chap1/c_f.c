736d159e7e99bb6b4265f6674aa3f61f9a7a8590
Sesha <seshasaicharla@gmail.com>
2018-02-18 17:21:28 +0530

    Corrected the loop execution.

--- chap1/c_f.c
+++ chap1/c_f.c
@@ -14,9 +14,9 @@ main()
     celsius = lower;
     printf("Celsius\tFarenheit\n");
     while (celsius <= upper){
-        fahr = ((9.0/5.0) * celsius) +32.0;
+        fahr = ((9.0/5.0) * celsius) + 32.0;
         printf("%3.0f\t\t%6.1f\n", celsius, fahr);
-        fahr = fahr + step;
+        celsius = celsius + step;
     }
 }
 
